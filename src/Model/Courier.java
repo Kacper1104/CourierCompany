930624065210520e57fb94cfefc1734b98ceb7e6
stud <stud@wiz.local>
2019-01-16 12:51:59 +0100

    edited advice and courier models

--- src/Model/Courier.java
+++ src/Model/Courier.java
@@ -4,4 +4,12 @@ package Model;
  * Created by Kacper on 12.01.2019.
  */
 public class Courier {
+	private int id;
+	private int lista_rozwozowa;
+	private String login;
+	private String haslo;
+	
+	public Courier(int id, int lista_rozwozowa, String login, String haslo){
+		return;
+	}
 }
