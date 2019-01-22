41926e6e2f69a08b5caee7abc3facc15f660e0a1
Tomasz Biadacz <t.biadacz@interia.pl>
2019-01-22 21:17:40 +0100

    fixed images

--- src/main/java/Presenter/View_Presenter.java
+++ src/main/java/Presenter/View_Presenter.java
@@ -1,21 +1,21 @@
 package Presenter;
 
 import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
-import javax.swing.text.html.ImageView;
-import java.awt.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 /**
  * Created by Kacper on 12.01.2019.
@@ -27,7 +27,7 @@ public class View_Presenter extends Application {
     Button wprowadz_przesylke_btn, utworz_liste_rozwozowa_btn, nadaj_przesylke_zwrotna_btn;
 
     @FXML
-    javafx.scene.image.ImageView koerta_imageView;
+    ImageView koerta_imageView;
 
 
     @FXML
@@ -60,6 +60,9 @@ public class View_Presenter extends Application {
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
 
+  //      Image image = new Image("file:src/koperta.png");
+//        koerta_imageView.setImage(image);
+//          D:\Desktop\STUDIA\Projektowanie Oprogramowania\Lab\Implementacja\CourierCompany\src
 
     }
 
